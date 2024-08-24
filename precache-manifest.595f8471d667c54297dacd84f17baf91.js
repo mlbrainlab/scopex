0a1f7e8013ec900f1e7a2b610e5e3f41c0c9bec4
Zinger <mnitpro@gmail.com>
2024-08-25 00:09:29 +0300

    Updates

---
+++ precache-manifest.595f8471d667c54297dacd84f17baf91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4867204c19012d98476130f366b52162",
+    "url": "/scopex/index.html"
+  },
+  {
+    "revision": "86d1382962b9c4158ebd",
+    "url": "/scopex/static/css/main.28f98ba3.chunk.css"
+  },
+  {
+    "revision": "c8ed5800d0ea5b152f24",
+    "url": "/scopex/static/js/2.8042a87e.chunk.js"
+  },
+  {
+    "revision": "86d1382962b9c4158ebd",
+    "url": "/scopex/static/js/main.a49d3a8b.chunk.js"
+  },
+  {
+    "revision": "0504c845ad9560040386",
+    "url": "/scopex/static/js/runtime~main.7b89c16a.js"
+  }
+]);
